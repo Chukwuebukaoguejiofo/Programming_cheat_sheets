c262a7a20bd845bfb1bd5a4bc6b2d38de810137c
Brian Spinos <brianspinos777@hotmail.com>
2016-04-13 20:28:36 -0700

    Update modules.rb

--- scripting_languages/ruby/modules.rb
+++ scripting_languages/ruby/modules.rb
@@ -19,3 +19,30 @@ end
 dude = Person.new
 
 dude.fly # 'flying'
+
+#------------------------
+
+module Foo
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def bar
+      puts 'class method'
+    end
+  end
+  
+  def foo
+    puts 'instance method'
+  end
+end
+
+class Baz
+  include Foo
+end
+
+Baz.bar # class method
+Baz.new.foo # instance method
+Baz.foo # NoMethodError: undefined method ‘foo’ for Baz:Class
+Baz.new.bar # NoMethodError: undefined method ‘bar’ for #<Baz:0x1e3d4>
