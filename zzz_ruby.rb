7eea5027ec07a754749e8df52e0724ab21142798
Brian Spinos <brianspinos777@hotmail.com>
2016-05-04 10:41:06 -0700

    Create zzz_ruby.rb

---
+++ zzz_ruby.rb
@@ -0,0 +1,15 @@
+# commentatios (que nao serao tratados como codigo)
+
+
+puts "hello"  # resultado: hello
+
+# criando uma funcao:
+def	andar
+  puts "estou andando"
+end
+
+# usando uma funcao:
+walk  # resultado:  estou andando
+
+
+
