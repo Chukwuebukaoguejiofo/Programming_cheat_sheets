1ce3aa62376f02b4a09b16a2f8219d118b072843
Brian Spinos <brianspinos777@hotmail.com>
2016-05-04 10:45:11 -0700

    Update zzz_ruby.rb

--- zzz_ruby.rb
+++ zzz_ruby.rb
@@ -11,9 +11,6 @@ end
 # usando uma funcao:
 walk  # resultado:  estou andando
 
-
-
-
 # classes (usadas para criar varios objetos do mesmo tipo)
 class Pessoa
 	def falar
@@ -28,7 +25,6 @@ end
 brian = Pessoa.new # criando um objeto
 brian.falar # resultado: estou falando
 brian.caminhar # estou caminhando
-
 
 erich = Pessoa.new # criando um objeto
 erich.falar # resultado: estou falando
