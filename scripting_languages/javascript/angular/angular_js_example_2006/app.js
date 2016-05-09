7af1f5f01b8d4ec35f8d75f308f363a943b5d5b8
Brian Spinos <brianspinos777@hotmail.com>
2016-05-09 07:09:01 -0700

    Update app.js

--- scripting_languages/javascript/angular/angular_js_example_2006/app.js
+++ scripting_languages/javascript/angular/angular_js_example_2006/app.js
@@ -1,5 +1,5 @@
 (function(){
-	// the `app` variable is private to this file
+	// the `app` variable is private to this file because of the closure
 
 	// adding another module as a dependency (it seems that the loading of the javascript files in the HTML do not matter)
 	// by adding another module as a dependency, I can use it's attributes, for example: the other module's controllers
