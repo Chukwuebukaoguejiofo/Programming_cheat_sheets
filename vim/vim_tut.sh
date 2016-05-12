3ce71cc654b7a1f95d5c670477a6cbec6a0cf286
Brian Spinos <brianspinos777@hotmail.com>
2016-05-12 11:06:22 -0700

    Create vim_tut.sh

---
+++ vim/vim_tut.sh
@@ -0,0 +1,40 @@
+# I will organize this latter...
+
+$ vi <file>
+
+
+
+
+# pattern find
+ESC  /foo
+
+
+#replace all
+
+ESC :%s/foo/bar/g
+
+
+
+
+# go to line #
+ESC :123
+
+
+
+# go to another file
+ESC :e /path/to/file
+
+
+
+
+
+
+
+
+# up, down, left, right   # use the arrows, duh!
+
+# faster right and left:    w   and b
+
+
+
+# copy word:        yw
