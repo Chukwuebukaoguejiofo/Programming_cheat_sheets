30037964e173b52c63eed89e563e9bd0ecebf93b
Brian Spinos <brianspinos777@hotmail.com>
2016-05-12 21:17:28 -0700

    Update vim_tut.sh

--- vim/vim_tut.sh
+++ vim/vim_tut.sh
@@ -1,40 +1,17 @@
 # I will organize this latter...
 
-$ vi <file>
+$ vi <file>  # edit a file
 
+ESC  /foo # pattern find
 
+ESC :%s/foo/bar/g #replace all
 
+ESC :123 # go to line number 123
 
-# pattern find
-ESC  /foo
-
-
-#replace all
-
-ESC :%s/foo/bar/g
-
-
-
-
-# go to line #
-ESC :123
-
-
-
-# go to another file
-ESC :e /path/to/file
-
-
-
-
-
-
-
+ESC :e /path/to/file # go to another file
 
 # up, down, left, right   # use the arrows, duh!
 
 # faster right and left:    w   and b
-
-
 
 # copy word:        yw
