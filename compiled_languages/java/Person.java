0dacb77557b48077460c1ac37f6a39b3da4fb26e
Brian Spinos <brianspinos777@hotmail.com>
2016-06-18 14:02:53 -0700

    Update Person.java

--- compiled_languages/java/Person.java
+++ compiled_languages/java/Person.java
@@ -1,40 +1,42 @@
-// test java
-
-
+// file name: Person.java
 
 //--------------------------------------------------------------- class
 public class Person {
-  String name;  // instance variable //(if its outside a function, its an instance varialbe)
+    String name;  // instance variable //(if its outside a function, its an instance varialbe)
   
-  // constructor
-  public Person(String name){
-      // 'name' is the parameter, 'this.name' is the instance variable! (thats the difference)
-      System.out.println("Name chosen is :" + name );
-      this.name = name;
-   }
+    /*
+     * constructor
+     */
+    public Person(String name){
+        // 'name' is the parameter, 'this.name' is the instance variable! (thats the difference)
+        System.out.println("Name chosen is: " + name );
+        this.name = name;
+    }
 
-   public void setName( String name ){
-       this.name = name;
-   }
+    public void setName( String name ){
+        this.name = name;
+    }
 
-   public void getName( ){
-       System.out.println( this.name );
-   }
+    public void getName( ){
+        System.out.println( this.name );
+    }
+  
+    public static void main(String []args){
+        /* Object creation */
+        Person brian = new Person( "brian" );
 
-   public static void main(String []args){
-      /* Object creation */
-      Person brian = new Person( "brian" );
+        /* Call class method to set puppy's age */
+        brian.setName( "erich" );
 
-      /* Call class method to set puppy's age */
-      brian.setName( "erich" );
+        /* Call another class method to get the Person's Name */
+        brian.getName( );
 
-      /* Call another class method to get puppy's Name */
-      brian.getName( );
-
-
-      /* You can access instance variable as follows as well */
-      System.out.println("Variable Value :" + brian.name );
-   }
+        /* 
+         * You can access instance variable as follows as well 
+         * but its better to use encapsulation
+         */
+        System.out.println("Variable Value :" + brian.name );
+    }
 }
 //--------------------------------------------------------------- abstract class
 
@@ -42,12 +44,9 @@ public class Person {
 //--------------------------------------------------------------- interface
 
 
-
-
-
 //--------------------------------------------------------------- compile
-// $ javac test.java
+// $ javac Person.java
 // run
-// $ java test
+// $ java Person
 
 
