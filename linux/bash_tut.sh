ac70435923868aa6e334698ca202fd8764f9f87d
Brian Spinos <brianspinos777@hotmail.com>
2016-06-25 16:48:28 -0700

    Create bash_tut.sh

---
+++ linux/bash_tut.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# commentss
+
+echo "hello world"  # print to the screen
+
+$ myVariable="hello"  # no space around the equals sign!
+
+declare -r FOO=5 # Declare a constant
+
+# Use arithmetic expansion for adding
+result=$((var1 + var2))
+
+# interpolation:
+echo "5 + 4 = $var3"
