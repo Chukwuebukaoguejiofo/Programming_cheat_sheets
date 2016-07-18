905bdba3350e20ea3bd9c1463233d1c7255feb80
Brian Spinos <brianspinos777@hotmail.com>
2016-07-18 10:50:17 -0700

    Create rotations.js

---
+++ #scripting_languages/javascript/data_structures/trees/rotations/rotations.js
@@ -0,0 +1,41 @@
+/* right rotate
+ *
+ *            X
+ *          /   \ 
+ *		   Y	 T3
+ *		  / \     
+ *		 T1  T2  
+ */
+function(root){
+	// identifying nodes
+	y = root.left;
+	t2 = y.right;
+
+	// actual rotation
+	y.right = x;
+	x.left = t2;
+
+	// new root
+	return y; 
+}
+
+/* left rotate
+ *
+ *             X
+ *           /   \ 
+ *		   T1	 Y
+ *		        / \     
+ *		       T2  T3  
+ */
+function(root){
+	// identifying nodes
+	y = root.right;
+	t2 = y.left;
+
+	// actual rotation
+	y.left = x;
+	x.right = t2;
+
+	// new root
+	return y; 
+}
