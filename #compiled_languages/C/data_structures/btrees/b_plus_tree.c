b634f03096ebad22ca3815a8bb17eaf38e95dcec
Brian Spinos <brianspinos777@hotmail.com>
2016-08-03 11:16:48 -0700

    Update b_plus_tree.c

--- #compiled_languages/C/data_structures/btrees/b_plus_tree.c
+++ #compiled_languages/C/data_structures/btrees/b_plus_tree.c
@@ -1,1 +1,6 @@
-// http://www.cburch.com/cs/340/reading/btree/
+/** 
+ * http://www.cburch.com/cs/340/reading/btree/
+ * 
+ * http://technicalpostsbyarifkhan.blogspot.com/2014/01/b-tree-data-structure-explained.html
+ * 
+ * /
