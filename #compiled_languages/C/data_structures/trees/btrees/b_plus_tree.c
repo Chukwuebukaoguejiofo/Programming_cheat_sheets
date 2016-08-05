c590cec9756807389d73d3b2e8a8c69ee1039243
Brian Spinos <brianspinos777@hotmail.com>
2016-08-05 09:20:17 -0700

    Update b_plus_tree.c

--- #compiled_languages/C/data_structures/trees/btrees/b_plus_tree.c
+++ #compiled_languages/C/data_structures/trees/btrees/b_plus_tree.c
@@ -7,8 +7,8 @@
  *    - PAGE is a chunck of data retrieved from DISK, then added to memory
  * 
  *  memory types:
- *    - 1. primary memory, main memory, internal memory, RAM, ROM... (all the same)
- *    - 2. secondary memory, external memory, disk...  (all the same)
+ *    - 1. primary memory, main memory, internal memory, temporary storage, RAM... (all the same)
+ *    - 2. secondary memory, external memory, permanent storage, disk...  (all the same)
  * 
  * 
  * 
