8b904a4ed9d339c1afdc4559929ebbea4223ef95
Brian Spinos <brianspinos777@hotmail.com>
2016-08-12 09:24:17 -0700

    Update test.js

--- #scripting_languages/javascript/data_structures/disjoint_sets/test.js
+++ #scripting_languages/javascript/data_structures/disjoint_sets/test.js
@@ -2,64 +2,83 @@
 
 
 function DisjointSet(n){
-	debugger;
-	this.size = n;
-	this.array = [0,1,2,3,4,5,6,7,8,9];
-	
+    var self = this;
+    
+    self.size = n;
+    self.array = [];
+    
 
-	this.union = union;
-	this.find = find;
+    self.union = union;
+    self.find = find;
+    self.print = print;
 
-	// (function makeSets(){
-	// 	for(var i = 0; i < this.size; i++){
-	// 		this.array[i] = i;
-	// 	}
-	// })();
+    function init(n){
+        for(var i = 0; i < self.size; i++){
+            self.array[i] = i;
+        }
+    }
+    init();
 
-	function union(x, y){
+    function union(x, y){        
+        var xParent = find(x);
+        var yParent = find(y);
 
-		debugger;
-		var xParent = find(x);
-		var yParent = find(y);
+        if(xParent > -1 && yParent > -1){
+            // both don't have rank
+            self.array[xParent] = yParent;
+            self.array[yParent] = -1;
+        }else if(xParent < 0 && yParent < 0){
+            // both have rank
+            if(xParent < yParent){
+                // x has higher rank
+                self.array[xParent] -= 1;
+                self.array[yParent] = yParent;
+            }else{
+                // y has higher rank
+                self.array[yParent] -= 1;
+                self.array[xParent] = yParent;
+            }
+        }else if(xParent < 0 && yParent > -1){
+            // only x has rank
+            self.array[yParent] = xParent;
+            self.array[xParent] -= 1;
+        }else if(xParent > -1 && yParent < 0){
+            // only y has rank
+            self.array[xParent] = yParent;
+            self.array[yParent] -= 1;
+        }
+    }
 
-		if(xParent > -1 && yParent > -1){
-			// both don't have rank
-			this.array[xParent] = yParent;
-			this.array[yParent] = -1;
-		}else if(xParent < 0 && yParent < 0){
-			// both have rank
-			if(xparent < yParent){
-				// x has higher rank
-				this.array[xParent] -= 1;
-				this.array[yParent] = yParent;
-			}else{
-				// y has higher rank
-				this.array[yParent] -= 1;
-				this.array[xParent] = yParent;
-			}
-		}else if(xParent < 0 && yParent > -1){
-			// only x has rank
-			this.array[yParent] = xParent;
-			this.array[xParent] -= 1;
-		}else if(xParent > -1 && yParent < 0){
-			// only y has rank
-			this.array[xParent] = yParent;
-			this.array[yParent] -= 1;
-		}
-	}
+    function find(x){
+        var parent = self.array[x];
 
-	function find(x){
-		debugger;
-		var parent = this.array[x];
-		while(parent > -1){
-			parent = this.array[parent];
-		}
+        if(x == self.array[x]){
+            return x;
+        }
 
-		return parent;
-	}
+        while(parent > -1){
+            parent = self.array[parent];
+        }
+
+        return parent;
+    }
+
+    function print(){
+        for(var i=0;i<self.array.length;i++){
+            console.log(i + ' [' + self.array[i] + '] ');
+        }
+    }
 }
 
 var set = new DisjointSet(10);
 
 
 set.union(2,3);
+
+
+set.print();
+
+set.union(2,3);
+
+
+set.print();
