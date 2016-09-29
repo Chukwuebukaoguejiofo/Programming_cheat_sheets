c09106f647afa1d02b8c79eb41ac980e11387247
Brian Spinos <brianspinos777@hotmail.com>
2016-09-29 14:24:45 -0700

    Update class_example.php

--- #scripting_languages/php/class_example.php
+++ #scripting_languages/php/class_example.php
@@ -63,63 +63,56 @@ MyClass::MyClass($name);  //
 
 
 
-
     // public: accessible inside/outside of the class definition
     // protected: accessible inside the classes definition and its child class definitions
     // private: accessible inside the class definition only
     class Foo {
+		
         private $fooPrivateField = "Foo private field\n";
         protected $fooProtectedField = "Foo protected field\n";
         public $fooPublicField = "Foo public field\n";
-
+		
         private function fooPrivateMethod(){
             echo "Foo private function\n";
         }
-
-        private function fooProtectedMethod(){
+        protected function fooProtectedMethod(){
             echo "Foo protected function\n";
         }
-
         public function fooPublicMethod(){
             echo "Foo public function\n";
         }
     }
 
     class Bar extends Foo{
+		
         private $barPrivateField = "Bar private field\n";
         protected $barProtectedField = "Bar protected field\n";
         public $barPublicField = "Bar public field\n";
-
+		
         private function barPrivateMethod(){
             echo "Bar private function\n";
         }
-
         protected function barProtectedMethod(){
             echo "Bar protected function\n";
         }
-
         public function barPublicMethod(){
             echo "Bar public function\n";
         }
-
-
         // to be used outside the class definition (it needs to be public)
         public function parentStuff(){
-            //echo $this->fooPrivateField . "\n";  // undefined
-            echo "\n";
+            //echo $this->fooPrivateField;  // undefined
             echo $this->fooProtectedField;
             echo $this->fooPublicField;
-
-            //$this->fooProtectedMethod();  // fatal error
-            // $this->fooPrivateMethod();  // fatal error
+			
+			// $this->fooPrivateMethod();  // fatal error
+            $this->fooProtectedMethod();
             $this->fooPublicMethod();
-            echo "\n";
         }
-
-
     }
-
+	
+	echo "----------------- bar\n";
     $bar = new Bar();
+
     //echo $bar->barPrivateField;      // fatal error
     //echo $bar->barProtectedField;    // fatal error
     echo $bar->barPublicField;
@@ -127,10 +120,32 @@ MyClass::MyClass($name);  //
     //$bar->barPrivateMethod();        // fatal error
     //$bar->barProtectedMethod();    // fatal error
     $bar->barPublicMethod();
+
+	echo "----------------- bar's parentStuff\n";
     $bar->parentStuff();
 
+	echo "----------------- bar accessing Foo methods and fields\n";
     //$bar->fooPrivateMethod();     // fatal error
     //$bar->fooProtectedMethod();   // fatal error
     $bar->fooPublicMethod();
+
+	// echo $bar->fooPrivateField; // Undefined 
+	// echo $bar->fooProtectedField; // Cannot access protected property
+	echo $bar->fooPublicField;
+
+// output:
+
+// ----------------- bar
+// Bar public field
+// Bar public function
+// ----------------- bar's parentStuff
+// Foo protected field
+// Foo public field
+// Foo protected function
+// Foo public function
+// ----------------- bar accessing Foo methods and fields
+// Foo public function
+// Foo public field
+
 
 ?>
