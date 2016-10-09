2fb9a9bf31268dd2590e0d358defa8234af086e8
Brian Spinos <brianspinos777@hotmail.com>
2016-10-08 22:54:54 -0700

    Update commands.sh

--- linux/commands.sh
+++ linux/commands.sh
@@ -1,7 +1,14 @@
-$ ssh root@72.14.183.208 # login into another machine.
+# login into another machine.
+$ ssh root@72.14.183.208 
 
-$ ssh root@72.14.183.208 "ls -la" # login into another machine and execute a command.
+# login into another machine and execute a command.
+$ ssh root@72.14.183.208 "ls -la" 
 
-$ spc -r root@72.14.183.208:/var/foo .  # secure copy from another computer to your current computer.
+# secure copy from another computer to your current computer.
+$ spc -r root@72.14.183.208:/var/foo .  
 
-$ rsync -r . root@72.14.183.208:/var/foo # sync the server files with what is in your current computer.
+# sync the server files with what is in your current computer.
+$ rsync -r . root@72.14.183.208:/var/foo 
+
+# creates a symlink (pointer to a file/folder) so you can treat the symlink just as the normal file/folder
+$ ln -s foo foo_pointer 
