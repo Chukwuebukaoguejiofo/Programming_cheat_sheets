f668a82c6052a749a25c460435f924376f9c8392
Brian Spinos <brianspinos777@hotmail.com>
2016-11-02 21:57:44 -0700

    Update radix_sort.rb

--- _scripting_languages/Ruby/algorithms/radix_sort.rb
+++ _scripting_languages/Ruby/algorithms/radix_sort.rb
@@ -1,5 +1,6 @@
 def radixSort(array)
     temp = []
+    result = []
 
     array.each do |x|
         if temp[x] == nil
@@ -13,12 +14,15 @@ def radixSort(array)
         if (x)
             x.times do
                 puts i
+                result.push(i)
             end
         end
     end
+    
+    return result
 end
 
 array = [5,4,6,3,7,2,8,1,9,0]
-radixSort(array)
+radixSort(array)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 p array
