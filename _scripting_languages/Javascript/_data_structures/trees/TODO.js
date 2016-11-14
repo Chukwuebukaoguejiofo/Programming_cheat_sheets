1ec83da7921bc6fc68aa17f46035d0668ad244ea
Brian Spinos <brianspinos777@hotmail.com>
2016-11-13 21:46:49 -0700

    Update TODO.js

--- _scripting_languages/Javascript/_data_structures/trees/TODO.js
+++ _scripting_languages/Javascript/_data_structures/trees/TODO.js
@@ -2,9 +2,9 @@
 
 /*
 - write pseudo code for rbt and avl tree.
-- write generic insertion code, iterative and recursive
+- write generic insertion code, iterative and recursive with comments
 - in rbt , cleanup
 
-
+- use other functions of the rbt, dont use recursion, but create another rbt that uses recursion
 
 */
