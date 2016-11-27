cc167ab19a4ed8d9cdc790bb4dc746fc6788bfd8
Brian Spinos <brianspinos777@hotmail.com>
2016-11-26 17:01:49 -0700

    Update disjoint_sets.rb

--- _scripting_languages/Ruby/data_structures/disjoint sets/disjoint_sets.rb
+++ _scripting_languages/Ruby/data_structures/disjoint sets/disjoint_sets.rb
@@ -1,8 +1,12 @@
 #
 # Disjoint sets (also called union-find algorithm)
-# all elements start off with a -1 value (which you can think of as the number of elements a set has!)
+# all elements start off with a -1 value
+# the rank of a root is only incremented if the two roots have the same rank
+# the rank is proportional to the height of the tree.
 #
 class DisjointSet
+    attr_accessor :array, :size
+    
     def initialize(size)
         @size = size
         @array = []
@@ -14,35 +18,47 @@ class DisjointSet
     def union(x, y)
         xP = find(x)
         yP = find(y)
-
-        if @array[xP] < @array[yP]
+        
+        if xP == yP 
+          return
+        end
+        
+        # remember that negative numbers indicate the rank
+        if @array[xP] < @array[yP] # in this case, xP has a greater rank!
             @array[y] = xP
-            @array[xP] -= 1
         elsif @array[xP] > @array[yP] 
             @array[x] = yP
-            @array[yP] -= 1
         elsif @array[xP] == @array[yP]
-            @array[x] = yP
-            @array[yP] -= 1
-        end   
+            @array[yP] = xP
+            @array[xP] -= 1 # only in this case we increment the rank!
+        end 
     end
-
+    
+    #
+    # find the parent using path compression
+    #
     def find(x)
-        if @array[x] < 0
-            return x
+        parent = @array[x]
+        if parent < 0 # when its a negative value, indicating a 'rank'
+            return x # return the parent, not the rank...
         end
-
-        # there was a bug whe I did not use the return on the function call...
-        return find(@array[x])
+        
+        # path compression 
+        # all children in the path will point directly to the root of the tree
+        @array[x] = find(parent) 
+        return @array[x]
     end
 
-    def print
+    def display
         @size.times do |index|
             puts "#{index} [#{@array[index]}]"
         end
     end
 end
 
+#
+# API
+#
 
 set = DisjointSet.new(10)
 
@@ -51,27 +67,27 @@ set.union(2,3)
 set.union(3,4)
 set.union(4,5)
 
-set.find(1) # 2
-set.find(2) # 2
-set.find(3) # 2
-set.find(4) # 2
-set.find(5) # 2
+set.find(1) # 1 (in group with '1' as representative)
+set.find(2) # 1 (in group with '1' as representative)
+set.find(3) # 1 (in group with '1' as representative)
+set.find(4) # 1 (in group with '1' as representative)
+set.find(5) # 1 (in group with '1' as representative)
 
-set.find(6) # 6
-set.find(7) # 7
-set.find(8) # 8
-set.find(9) # 9
+set.find(6) # 6 (in group with '6' as representative)
+set.find(7) # 7 (in group with '7' as representative)
+set.find(8) # 8 (in group with '8' as representative)
+set.find(9) # 9 (in group with '9' as representative)
 
+set.display
 
-set.print
 # OUTPUT:
 #
 # 0 [-1]
-# 1 [2]
-# 2 [-5]
-# 3 [2]
-# 4 [2]
-# 5 [2]
+# 1 [-2]
+# 2 [1]
+# 3 [1]
+# 4 [1]
+# 5 [1]
 # 6 [-1]
 # 7 [-1]
 # 8 [-1]
