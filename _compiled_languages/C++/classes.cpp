4fe372a3b8f748ebe8cff0082e62b6c2069b030e
Brian Spinos <brianspinos777@hotmail.com>
2016-11-28 00:09:03 -0700

    Create classes.cpp

---
+++ _compiled_languages/C++/classes.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+class Person{
+    public:
+    
+    // attributes
+    string name;
+    int age;
+    
+    Person(string name, int age); // constructor
+    
+    // methods
+    string sayHello();
+    string sayName();
+    
+    private:
+    
+    string password;
+};
+
+// constructor implementation (no return type allowed)
+Person::Person(string name, int age){
+    this->name = name;
+    this->age = age;
+}
+
+string Person::sayHello(){
+    return "Hello there";
+}
+
+string Person::sayName(){
+    string x = "my name is ";
+    x += this->name;
+    return x;
+}
+
+int main(){
+    
+   // instantiation
+   Person * brian = new Person("Brian", 28);
+
+   cout << brian->name << endl; 
+   cout << brian->age << endl; 
+   cout << brian->sayHello() << endl; 
+   cout << brian->sayName() << endl; 
+   
+   delete brian;
+   
+   return 0;
+}
+
