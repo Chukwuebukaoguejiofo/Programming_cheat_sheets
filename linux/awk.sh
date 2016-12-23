1a532cbe7c5d027be59f172dcb389de6f2ba98da
Brian Spinos <brianspinos777@hotmail.com>
2016-12-22 18:37:05 -0700

    Create awk.sh

---
+++ linux/awk.sh
@@ -0,0 +1,20 @@
+#---------------------- brian.txt ----------------
+# name : brian : age : 21 : address : 456 foobar
+# name : brian : age : 22 : address : 456 foobar
+# name : brian : age : 23 : address : 456 foobar
+# name : brian : age : 24 : address : 456 foobar
+# name : brian : age : 25 : address : 456 foobar
+# name : erich : age : 26 : address : 123 foobar
+# name : rich : age : 40 : address : 789 foobar
+#-------------------------------------------------
+
+$ awk -F: '{print $2  $4}' brian.txt 
+
+###### OUTPUT:
+# brian  21 
+# brian  22 
+# brian  23 
+# brian  24 
+# brian  25 
+# erich  26 
+# sandra  40 
