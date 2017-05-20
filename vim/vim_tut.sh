f8db781f1dbf5075e1fc242ed22a143134e99aa2
Brian Spinos <brianspinos777@hotmail.com>
2017-05-20 14:58:49 -0700

    Update vim_tut.sh

--- vim/vim_tut.sh
+++ vim/vim_tut.sh
@@ -2,6 +2,18 @@
 
 $ vi <file>  # edit a file
 
+# faster right and left:    w   and b
+
+1yy  # copy 1 line
+
+p  # paste
+
+u  # undo
+
+CTRL r  # re-do
+
+1dd # delete 1 line
+
 ESC  /foo # pattern find
 
 ESC :%s/foo/bar/g #replace all
@@ -12,6 +24,6 @@ ESC :e /path/to/file # go to another file
 
 # up, down, left, right   # use the arrows, duh!
 
-# faster right and left:    w   and b
+
 
 # copy word:        yw
