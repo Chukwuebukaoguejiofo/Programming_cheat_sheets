c665163a4a9ee154fae2588ac006dd84110cf54f
Brian Spinos <brianspinos777@hotmail.com>
2017-06-25 10:45:14 -0700

    Update strategy.rb

--- _scripting_languages/Ruby/design_patterns/strategy.rb
+++ _scripting_languages/Ruby/design_patterns/strategy.rb
@@ -2,12 +2,7 @@
 # Strategy Patterns
 #     - have an object receive another object that encapsulates an algorithm.
 #
-# strategy: 1 class, and many strategies
-# visitor: many classes, and many visitors with a method to do work for each class
 
-# receives a strategy and uses its methods
-# receives a visitor  and the visitor does its thing
-# class is wrapped and its methods are inhanced
 
 class Calculation
     attr_accessor :strategy
