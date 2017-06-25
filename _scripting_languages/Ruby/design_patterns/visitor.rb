4ccae60670a47431db114a5d8a046dc696a13087
Brian Spinos <brianspinos777@hotmail.com>
2017-06-24 21:01:07 -0700

    Update visitor.rb

--- _scripting_languages/Ruby/design_patterns/visitor.rb
+++ _scripting_languages/Ruby/design_patterns/visitor.rb
@@ -1,5 +1,7 @@
-
-#------------------------------------------------------ visitor
+#
+# Visitor Patterns
+#     - have and object1 that accept object2, object2 will change the sate of object1
+#
 # strategy: 1 class, and many strategies
 # visitor: many classes, and many visitors with a method to do work for each class
 
@@ -7,19 +9,44 @@
 # receives a visitor  and the visitor does its thing
 # class is wrapped and its methods are inhanced
 
+#
+# Class that will be visited
+#
+class Car 
+    attr_accessor :gear
 
-class Car # visitee
+    def initialize
+        @gear = nil
+    end
+
+    #
+    # The visitor will enter this object and make changes to it.
+    #
     def accept(visitor)
         visitor.visit(self)
     end
 
+    def change_gear(gear)
+        @gear = gear
+    end
 end
 
-class Person # visitor
+#
+# Visitor
+#
+class Person 
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
     def visit(car)
-        puts "hey #{car} !"
+        puts "#{@name} is changing gears in the car"
+        car.change_gear(1)
     end
 end
 
 car = Car.new
-car.accept(Person.new)
+brian = Person.new('Brian')
+car.accept(brian) # Brian is changing gears in the car
