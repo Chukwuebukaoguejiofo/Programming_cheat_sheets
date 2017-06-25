6cf26e10cedf47c830dddfa4ce1e35df029f75aa
Brian Spinos <brianspinos777@hotmail.com>
2017-06-24 20:17:33 -0700

    Update decorator.rb

--- _scripting_languages/Ruby/design_patterns/decorator.rb
+++ _scripting_languages/Ruby/design_patterns/decorator.rb
@@ -1,18 +1,42 @@
-
-
-#------------------------------------------------------ decorator
+#
+# Decorator
+# - an object is wrapped inside another object, 
+#   and the inner object's methods are inhanced by the wrapper object's methods
+#
 # strategy: 1 class, and many strategies
 # visitor: many classes, and many visitors with a method to do work for each class
-
+#
 # receives a strategy and uses its methods
 # receives a visitor  and the visitor does its thing
-# class is wrapped and its methods are inhanced
+# 
+
+#
+# The wrapper class
+#
+class ShapeDecorator
+    attr_accessor :shape
+
+    def initialize(shape)
+        @shape = shape
+    end
+
+    #
+    # method that will inhance the inner object's method
+    #
+    def draw
+        @shape.draw
+        puts 'and more!'
+    end
+end
+
+#
+# The classes that will be wrapped
+#
 
 class Circle
     def draw
         puts 'circle'
     end
-
 end
 
 class Square
@@ -21,24 +45,15 @@ class Square
     end
 end
 
-
-class ShapeDecorator
-    def initialize(shape)
-        @shape = shape
-    end
-
-    def draw
-        @shape.draw
-        puts 'and more!'
-    end
-end
+#======= Usage:
 
 circle = Circle.new
 square = Square.new
 
 shapeDecorator1 = ShapeDecorator.new(circle)
-shapeDecorator1.draw
+shapeDecorator1.draw # circle and more!
 
 shapeDecorator2 = ShapeDecorator.new(square)
-shapeDecorator2.draw
+shapeDecorator2.draw # square and more!
+
 
