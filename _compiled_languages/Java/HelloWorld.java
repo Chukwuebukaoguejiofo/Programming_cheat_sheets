88e915dcc42f1d977ab2bea1a39d9841030bc86f
Brian Spinos <brianspinos777@hotmail.com>
2017-06-25 13:13:18 -0700

    Update HelloWorld.java

--- _compiled_languages/Java/HelloWorld.java
+++ _compiled_languages/Java/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
      * all java files need to declare a class
      * you need a main function called 'main' in order to run the code.
      */
-    public static void main(String []args) {
+    public static void main(String[] args) {
        System.out.println("Hello World");
     }
 } 
