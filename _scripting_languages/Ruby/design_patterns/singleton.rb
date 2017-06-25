387ff8fe834cd22d94d801a3ff0339ad30cec519
Brian Spinos <brianspinos777@hotmail.com>
2017-06-25 11:26:12 -0700

    Update singleton.rb

--- _scripting_languages/Ruby/design_patterns/singleton.rb
+++ _scripting_languages/Ruby/design_patterns/singleton.rb
@@ -1,10 +1,10 @@
-
 #
 # Singleton Pattern
 #     - Uses only one instance of a class
 #
 
 class President
+    attr_accessor :name
 
     #create an object of President
     @@instance = President.new
@@ -19,14 +19,26 @@ class President
     end
 
     def say_hello()
-        puts 'Good morning America!'
+        puts "#{@name}: Good morning America!"
     end
 end
 
 #
+# We cannot instantiate directly
 # president = President.new # Error!
 #
 
 # Get the only object available
 president = President.get_instance
-president.say_hello # 'Good morning America!'
+president.name = 'Trump'
+president.say_hello # Trump: Good morning America!
+
+#---
+
+president1 = President.get_instance
+president2 = President.get_instance
+
+president1.object_id == president2.object_id # true
+
+
+
