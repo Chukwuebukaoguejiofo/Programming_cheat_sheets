003f25eb1befd67d0040c1c3c66fc91f3b5f04d2
Brian Spinos <brianspinos777@hotmail.com>
2017-06-25 21:22:50 -0700

    Update and rename classes.py to _example.py

--- _scripting_languages/Python/_example.py
+++ _scripting_languages/Python/_example.py
@@ -2,14 +2,23 @@
 # Person class
 #
 class Person:
-    "this is a person class"
-    personCount = 0 # class attribute
+    "This is the documentation for the person class"
+
+    # Class variable
+    personCount = 0 
     
+    # Constructor
     def __init__(self, name, age):
         self.name = name 
         self.age = age 
         Person.personCount += 1 
     
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
     def sayHello(self):
         print("hello there! my name is: " + self.name)
         
@@ -23,10 +32,18 @@ class Person:
 brian = Person("Brian Spinos", 28)
 brian.sayHello()
 brian.sayAge()
+brian.set_name("Ryan")
+print( brian.get_name() )
+
 
 erich = Person("erich Spinos", 25)
 erich.sayHello()
 erich.sayAge()
+brian.set_name("Derik")
+print( brian.get_name() )
+
 
 print(Person.personCount) # 2
-print(Person.__doc__) # "this is a person class"
+print(Person.__doc__) # 'This is the documentation for the person class'
+
+
