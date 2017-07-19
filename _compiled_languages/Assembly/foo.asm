b4a6432450f3521103b645c1fa59d59c3c3ff4d6
Brian Spinos <brianspinos777@hotmail.com>
2017-07-18 21:41:31 -0700

    Update foo.asm

--- _compiled_languages/Assembly/foo.asm
+++ _compiled_languages/Assembly/foo.asm
@@ -1,3 +1,9 @@
+;
+; NASM 64bit
+;
+; filename: foo.asm
+;
+; ============================ How to run the code
 ; # Generate object file from assembly:
 ; nasm -f macho64 -o foo.o foo.asm
 ;
@@ -7,56 +13,128 @@
 ; # Run executable:
 ; ./foo
 
+
+
+
+;
+; ============================ REGISTERS
+; rax - What to do (exit, read, write)
+; rdi - Where to write to (stdout, ...)
+; rsi - The data
+; rdx - Data length
+;
+
+
+;
+; To make our life easy, lets define some variables
+;
 %define SYSCALL_WRITE 0x2000004
 %define SYSCALL_EXIT  0x2000001
+%define STDOUT        1
+
+
+; global start ; This call could be here also? where is the right place?
+
+
+;
+; ============================ Data section (variables)
+;
 
 section .data
-    ; to use escape sequences, use backticks
-    full_name db `Brian Spinos\n\n\n`
 
-    ; get the length of the string ; "$" means "here"
-    full_name_length equ $ - full_name
+msg1: db "Hello, world", 10
+.len: equ $ - msg1
 
-    foo db `foobar\n`
+msg2: db "Hello, world (again)", 10
+.len: equ $ - msg2
 
-    ; get the length of foo string ; "$" means "here"
-    foo_length equ $ - foo
+msg3: db "Hello, world (once more)", 10
+.len: equ $ - msg3
+
+; to use \n, use backticks
+msg4 db `Hello, world (one last time)\n\n`
+; get the length of the string ; "$" means "here"
+msg4_len equ $ - msg4
+
+
+;
+; ============================ BSS section (constants?)
+;
+
 
 section .bss
-    ; this section is for constants?
+; this section is for constants?
+
+;
+; ============================ Text section (code)
+;
+
 
 section .text
-    global start ; the main function name
 
-    start:
-        ; write 'Brian Spinos'
-        mov rax, SYSCALL_WRITE
-        mov rdi, 1 ; stdout
-        mov rsi, full_name
-        mov rdx, full_name_length
-        syscall ; call the kernel
-
-        ; write 'foobar'
-        mov rax, SYSCALL_WRITE
-        mov rdi, 1 ; stdout
-        mov rsi, foo
-        mov rdx, foo_length
-        syscall ; call the kernel
-
-        ; exit my program
-        mov rax, SYSCALL_EXIT
-        mov rdi, 0
-        syscall ; call the kernel
+global start ; The main function name
 
 
-; REGISTERS
-; rsi: what to wite to output?
-; rdx: length of output?
-; rax: what to do (exit, read, write)
-; rdi: where to write to, like stdout ?
+;
+; The main function (it can be called anything)
+;
+start:
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    mov rsi, msg1
+    mov rdx, msg1.len
+    syscall
+
+    call myFunc2
+    call myFunc3
+    call myFunc4
 
 
-; rax - temporary register; when we call a syscal, rax must contain syscall number ?
-; rdi - used to pass 1st argument to functions ?
-; rsi - pointer used to pass 2nd argument to functions ?
-; rdx - used to pass 3rd argument to functions ?
+    ;
+    ; Exit from main function, a must
+    ;
+    mov rax, SYSCALL_EXIT
+    mov rdi, 0
+    syscall
+
+
+myFunc2:
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    mov rsi, msg2
+    mov rdx, msg2.len
+    syscall
+    ret ; Return from function, a must
+
+myFunc3:
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    mov rsi, msg3
+    mov rdx, msg3.len
+    syscall
+    ret ; Return from function, a must
+
+myFunc4:
+    mov rax, SYSCALL_WRITE
+    mov rdi, STDOUT
+    mov rsi, msg4
+    mov rdx, msg4_len
+    syscall
+    ret ; Return from function, a must
+
+
+
+;
+; ============================ Output
+;
+
+; Hello, world
+; Hello, world (again)
+; Hello, world (once more)
+; Hello, world (one last time)
+
+
+
+;
+; ============================ End
+;
