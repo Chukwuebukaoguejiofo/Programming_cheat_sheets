4f665b2fcaf6531adf20c62d5a5137c43668f4cb
Brian Spinos <brianspinos777@hotmail.com>
2020-09-22 21:06:46 -0700

    Update QuickSort.java

--- _compiled_languages/Java/_data-structures-and-algorithms/__sorting-algorithms/QuickSort.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/__sorting-algorithms/QuickSort.java
@@ -1,100 +1,61 @@
 package com.example;
 
 import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
-class QuickSort{
-    int[] array;
-
-    public void sort(int[] array){
-        this.array=array;
-        sort(0, array.length-1);
+class QuickSort {
+    public static void sort(int[] array) {
+        sort(array, 0, array.length - 1);
     }
 
-    public void sort(int start, int end){
-        if(start >= end) return;
-        int sortedPosition = partition(start, end);
-        sort(start, sortedPosition-1);
-        sort(sortedPosition+1, end);
+    public static void sort(int[] array, int start, int end) {
+        if (start >= end) return;
+        int sortedPosition = partition(array, start, end);
+        sort(array, start, sortedPosition - 1);
+        sort(array, sortedPosition + 1, end);
     }
 
-    public int partition(int left, int right){
-        if(left == right) return left;
-        int pivot = array[right]; // we could select a random index, but we need to move it to the far right (or far left?)
+    public static int partition(int[] array, int left, int right) {
+        if (left == right) return left;
+
+        /**
+         * we could select a random index,
+         * but we need to move it to the far right (or far left?)
+         */
+        int pivot = array[right];
         int i = left;
-        int j = right -1;
+        int j = right - 1;
 
-        while(i<=j){
-            while(array[i]<pivot) i++;
-            while(i<=j && array[j]>pivot) j--;
+        while (i <= j) {
+            while (array[i] < pivot) i++;
+            while (i <= j && array[j] > pivot) j--;
 
-            if (i<=j){
-                swap(i,j);
+            if (i <= j) {
+                swap(array, i, j);
                 i++;
                 j--;
             }
         }
 
-        swap(i, right);
+        swap(array, i, right);
         return i;
     }
 
-    private void swap(int i, int j){
+    private static void swap(int[] array, int i, int j) {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 }
 
-public class Foo {
+public class QuickSortExample {
     public static void main(String[] args) {
-
-        // Input
         int[] array = new int[]{
-                1, 0, 5, 2, 3, 10, 1, 1, 9, 6, 8, 4, 7
+                5, 6, 4, 7, 3, 8, 2, 9, 1, 0
         };
-        System.out.println("Input:    " + Arrays.toString(array));
 
-        // Setup expectation
-        int[] array2 = array.clone();
-        Arrays.sort(array2);
-        String expected = Arrays.toString(array2);
+        QuickSort.sort(array);
 
-        // Act
-        QuickSort quickSort = new QuickSort();
-        quickSort.sort(array);
-
-        // Check
-        String actual = Arrays.toString(array);
-        System.out.println("Expected: " + expected);
-        System.out.println("Actual:   " + actual);
-
-        if (actual.equals(expected)){
-            System.out.println("SUCCESS");
-        }else{
-            System.out.println("FAIL");
-        }
-    }
-
-    static int[] shuffle(int[] ar) {
-        Random rnd = ThreadLocalRandom.current();
-        for (int i = ar.length - 1; i > 0; i--) {
-            int index = rnd.nextInt(i + 1);
-            int a = ar[index];
-            ar[index] = ar[i];
-            ar[i] = a;
-        }
-        return ar;
+        System.out.println(Arrays.toString(array));
+        //=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
 }
-
-
-/* OUTPUT
-
-Input:    [1, 0, 5, 2, 3, 10, 1, 1, 9, 6, 8, 4, 7]
-Expected: [0, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-Actual:   [0, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-SUCCESS
-
-*/
