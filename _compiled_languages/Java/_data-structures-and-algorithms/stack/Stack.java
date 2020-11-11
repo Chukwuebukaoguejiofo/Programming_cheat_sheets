6758b05fa81c0872f4cbcef8c2d89f5fdbe5e95c
Brian Spinos <brianspinos777@hotmail.com>
2020-11-10 23:17:31 -0700

    Update Stack.java

--- _compiled_languages/Java/_data-structures-and-algorithms/stack/Stack.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/stack/Stack.java
@@ -1,52 +1,108 @@
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
+package com.example;
+
+class Node {
+    private int key;
+    private Node next;
+
+    public Node(int key) {
+        this.key = key;
+        next = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
 
-class Stack{
-    Node top;
+class Stack {
+    private Node top;
 
-    boolean isEmpty(){
+    public Stack() {
+    }
+
+    public void push(int key) {
+        Node node = new Node(key);
+        if (top == null) {
+            top = node;
+            return;
+        }
+
+        node.setNext(top);
+        top = node;
+    }
+
+    public Node pop() throws Exception {
+        if (top == null) {
+            throw new Exception("INFO::1002 Stack is empty");
+        }
+
+        Node node = top;
+        top = top.getNext();
+        return node;
+    }
+
+    public boolean isEmpty() {
         return top == null;
     }
 
-    int peek(){
-        return top.data; // exception?
+    public Node peek() throws Exception {
+        if (top == null) {
+            throw new Exception("INFO::1001 Stack is empty");
+        }
+        return top;
     }
-    void push(int data){
-        Node node = new Node(data);
-        node.next = top;
-        top = node;
+
+    public void print() {
+        Node currNode = top;
+        while (currNode != null) {
+            System.out.print(currNode.getKey() + " -> ");
+            currNode = currNode.getNext();
+        }
+        System.out.println("");
     }
-    int pop(){
-        int data = top.data;
-        top = top.next;
-        return data;
-    }
+
 }
 
-public class Main{
+public class StackExample {
     public static void main(String[] args) {
-        Stack s = new Stack();
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.print();
+        System.out.println("stack.isEmpty() = " + stack.isEmpty());
 
-        s.push(3);
-        s.push(2);
-        s.push(1);
 
-        System.out.println("next: " + s.pop());
-        System.out.println("next: " + s.pop());
-        System.out.println("next: " + s.pop());
+        try {
+            stack.pop();
+            stack.print();
+
+            System.out.println("stack.peek() = " + stack.peek().getKey());
+
+            stack.pop();
+            stack.print();
+
+            stack.pop();
+            stack.print();
+
+            System.out.println("stack.isEmpty() = " + stack.isEmpty());
+
+            //stack.peek(); //  Stack is empty
+            //stack.pop(); //  Stack is empty
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 }
-
-/*
-OUTPUT:
-
-next: 1
-next: 2
-next: 3
-
- */
