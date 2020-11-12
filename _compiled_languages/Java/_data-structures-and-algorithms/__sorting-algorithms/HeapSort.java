d107b5425e4671eada5050c6be9d0ddf7cf61f49
Brian Spinos <brianspinos777@hotmail.com>
2020-11-11 18:30:38 -0700

    Update HeapSort.java

--- _compiled_languages/Java/_data-structures-and-algorithms/__sorting-algorithms/HeapSort.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/__sorting-algorithms/HeapSort.java
@@ -2,117 +2,89 @@ package com.example;
 
 import java.util.Arrays;
 
-/**
- * insertion to heap is bottom up
- * removal from heap is top, swap with bottom
- */
 class HeapSort {
-    private static int[] arr;
+    private int[] array;
 
-    public static void sort(int[] arr) {
-        if (arr.length == 1) return;
-        HeapSort.arr = arr;
-        buildMaxHeap();
-        sort();
+    public HeapSort(int[] array) {
+        this.array = array;
     }
 
-    public static void sort() {
-        for (int i = arr.length - 1; i >= 1; i--) { // from last index to index 1
-            swap(0, i);
-            heapifyUntil(i);
+    public void sort() {
+        if (array.length < 2) return;
+
+        // create heap
+        for (int i = 0; i < array.length; i++) {
+            bubbleUp(i);
+        }
+
+        // swap highest values to end or array
+        for (int i = 0; i < array.length; i++) {
+            int currLastIndex = array.length - 1 - i;
+            swap(0, currLastIndex);
+            bubbleDown(currLastIndex);
         }
     }
 
-    private static void heapifyUntil(int i) {
-        siftDownUntil(i); // i is exclusive
-    }
+    //
+    // Helpers
+    //
 
-    private static void buildMaxHeap() {
-        for (int i = 1; i < arr.length; i++) { // start at second item
-            siftUp(i);
+    private int PI(int i){ return (i-1)/2;}
+    private int LI(int i){ return 2 * i + 1;}
+    private int RI(int i){ return 2 * i + 2;}
+    private boolean hasP(int i){ return PI(i) >= 0;}
+    private boolean hasL(int i, int size){ return LI(i) < size;}
+    private boolean hasR(int i, int size){ return RI(i) < size;}
+    private int PV(int i){ return array[PI(i)]; }
+    private int LV(int i){ return array[LI(i)]; }
+    private int RV(int i){ return array[RI(i)]; }
+
+    private void bubbleUp(int i) {
+        while (i >= 0 && hasP(i) && PV(i) < array[i]) {
+            swap(PI(i), i);
+            i = PI(i);
         }
     }
 
-    private static void siftUp(int i) { // i will crawl up
-        while (hasParent(i) && arr[getParentIndex(i)] < arr[i]) {
-            swap(i, getParentIndex(i));
-            i = getParentIndex(i);
-        }
-    }
-
-    private static void siftDownUntil(int j) { // i will crawl down
+    // currLastIndex will NOT be touched/swapped
+    private void bubbleDown(int currLastIndex) {
         int i = 0;
-        while (hasLeftChild(i, j)) {
-            if (hasRightChild(i, j) && (getRightValue(i) > getLeftValue(i))) {
-                int rci = getRightChildIndex(i);
-                swap(i, rci);
-                i = rci;
-                continue;
+
+        while (hasL(i, currLastIndex)) {
+            int gci = LI(i);
+            if (hasR(i, currLastIndex) && RV(i) > LV(i)) {
+                gci = RI(i);
             }
 
-            int lci = getLeftChildIndex(i);
-            if (arr[lci] > arr[i]) {
-                swap(i, lci);
+            if (array[i] < array[gci]) {
+                swap(i, gci);
+                i = gci;
+            } else {
+                break;
             }
-            i = lci;
         }
     }
 
-    private static int getLeftChildIndex(int i) {
-        return (2 * i + 1);
-    }
-
-    private static int getRightChildIndex(int i) {
-        return (2 * i + 2);
-    }
-
-    private static int getLeftValue(int i) {
-        return arr[getLeftChildIndex(i)];
-    }
-
-    private static int getRightValue(int i) {
-        return arr[getRightChildIndex(i)];
-    }
-
-//    private static boolean hasRightChild(int i) {
-//        return getRightChildIndex(i) < arr.length;
-//    }
-
-    private static boolean hasRightChild(int i, int j) {
-        return getRightChildIndex(i) < j;
-    }
-
-//    private static boolean hasLeftChild(int i) {
-//        return getLeftChildIndex(i) < arr.length;
-//    }
-
-    private static boolean hasLeftChild(int i, int j) {
-        return getLeftChildIndex(i) < j;
-    }
-
-    private static int getParentIndex(int i) {
-        return (i - 1) / 2;
-    }
-
-    private static boolean hasParent(int i) {
-        return (i - 1) / 2 >= 0;
-    }
-
-    private static void swap(int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    private void swap(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
 
 public class HeapSortExample {
     public static void main(String[] args) {
-        int[] arr = {
-                5, 6, 4, 7, 3, 8, 2, 9, 1, 0
-        };
-
-        HeapSort.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        //=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        int[] arr = {5, 4, 6, 3, 7, 2, 8, 1, 0, 9};
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+        HeapSort heapSort = new HeapSort(arr);
+        heapSort.sort();
+        System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
     }
 }
+
+/* OUTPUT
+
+Arrays.toString(arr) = [5, 4, 6, 3, 7, 2, 8, 1, 0, 9]
+Arrays.toString(arr) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+*/
