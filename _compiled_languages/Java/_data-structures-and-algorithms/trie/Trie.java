4449ae8154bebcb6a4725bcf7aebc4c59e1e955f
Brian Spinos <brianspinos777@hotmail.com>
2020-11-11 17:32:44 -0700

    Update Trie.java

--- _compiled_languages/Java/_data-structures-and-algorithms/trie/Trie.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/trie/Trie.java
@@ -1,71 +1,93 @@
 package com.example;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 class TrieNode {
-    Map<Character, TrieNode> map = new HashMap<>();
-    boolean isEndOfWord = false;
+    private Map<Character, TrieNode> letters;
+    private boolean isWord;
 
-    void add(String s) {
-        char[] letters = s.toCharArray();
-        TrieNode currentNode = this;
-        TrieNode nextNode = null;
-
-        for (int i = 0; i < letters.length; i++) {
-            char currentLetter = letters[i];
-            nextNode = currentNode.map.get(currentLetter);
-            if (nextNode == null) {
-                nextNode = new TrieNode();
-                currentNode.map.put(currentLetter, nextNode);
-            }
-            currentNode = nextNode;
-        }
-
-        nextNode.isEndOfWord = true;
+    public TrieNode() {
+        letters = new HashMap<>();
     }
 
-    boolean hasWord(String s) {
-        char[] letters = s.toCharArray();
-        TrieNode currentNode = this;
-        TrieNode nextNode = null;
+    public Map<Character, TrieNode> getLetters() {
+        return letters;
+    }
 
-        for (int i = 0; i < letters.length; i++) {
-            char currentLetter = letters[i];
-            nextNode = currentNode.map.get(currentLetter);
-            if (nextNode == null) {
+    public void setLetters(Map<Character, TrieNode> letters) {
+        this.letters = letters;
+    }
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord(boolean word) {
+        isWord = word;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void add(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            TrieNode next = curr.getLetters().get(c);
+            if (next == null) {
+                TrieNode tn = new TrieNode();
+                curr.getLetters().put(c, tn);
+                curr = tn;
+            } else {
+                curr = next;
+            }
+        }
+        curr.setWord(true);
+    }
+
+    public boolean has(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            TrieNode next = curr.getLetters().get(c);
+            if (next == null) {
                 return false;
             } else {
-                // has current letter
-                currentNode = nextNode;
+                curr = next;
             }
         }
 
-        if (nextNode.isEndOfWord) {
-            return true;
-        }
-
-        return false;
+        return curr.isWord();
     }
 }
 
-public class Trie {
+public class TrieExample {
     public static void main(String[] args) {
-        TrieNode root = new TrieNode();
-
-        String[] words = {
-                "brian",
-                "ana",
-                "erich",
-                "bia"
-        };
-
-        for (int i = 0; i < words.length; i++) {
-            root.add(words[i]);
-        }
-
-        System.out.println("result: " + root.hasWord("bria")); // false
-        System.out.println("result: " + root.hasWord("brian")); // true
-        System.out.println("result: " + root.hasWord("brians")); // false
+        Trie trie = new Trie();
+        trie.add("brian");
+        trie.add("ana");
+        System.out.println("trie.has('brian') = " + trie.has("brian"));
+        System.out.println("trie.has('ana') = " + trie.has("ana"));
+        System.out.println("trie.has('bri') = " + trie.has("bri"));
+        System.out.println("trie.has('an') = " + trie.has("an"));
+        System.out.println("trie.has('briana') = " + trie.has("briana"));
+        System.out.println("trie.has('anan') = " + trie.has("anan"));
     }
 }
+
+/*
+
+trie.has('brian') = true
+trie.has('ana') = true
+
+trie.has('bri') = false
+trie.has('an') = false
+trie.has('briana') = false
+trie.has('anan') = false
+
+*/
