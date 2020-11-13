635a348e133356a0b32347c7047aaddbe87943db
Brian Spinos <brianspinos777@hotmail.com>
2020-11-12 19:36:45 -0700

    Update Heap.java

--- _compiled_languages/Java/_data-structures-and-algorithms/heap/Heap.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/heap/Heap.java
@@ -1,32 +1,40 @@
+package com.example;
+
 import java.util.Arrays;
 
 class Heap{
-    private int capacity = 10;
-    private int size = 0;
-    int[] items = new int[capacity];
+    private int capacity;
+    private int size;
+    int[] items;
+
+    public Heap(int capacity){
+        this.capacity = capacity;
+        size = 0;
+        items = new int[capacity];
+    }
 
     private int getLeftChildIndex(int parentIndex){ return 2 * parentIndex + 1; }
     private int getRightChildIndex(int parentIndex){ return 2 * parentIndex + 2; }
     private int getParentIndex(int childIndex){ return (childIndex - 1) / 2; }
 
-    private boolean hasLeftChild(int index){ return getLeftChildIndex(index) < size; }
-    private boolean hasRightChild(int index){ return getRightChildIndex(index) < size; }
-    private boolean hasParent(int index){ return getParentIndex(index) >= 0; }
+    private boolean hasLeftChild(int i){ return getLeftChildIndex(i) < size; }
+    private boolean hasRightChild(int i){ return getRightChildIndex(i) < size; }
+    private boolean hasParent(int i){ return i > 0; } // HACK!
 
-    private int getLeftChildValue(int index){ return items[getLeftChildIndex(index)]; }
-    private int getRightChildValue(int index){ return items[getRightChildIndex(index)]; }
-    private int getParentValue(int index){ return items[getParentIndex(index)]; }
+    private int getLeftChildValue(int i){ return items[getLeftChildIndex(i)]; }
+    private int getRightChildValue(int i){ return items[getRightChildIndex(i)]; }
+    private int getParentValue(int i){ return items[getParentIndex(i)]; }
 
-    private void swap(int indexOne, int indexTwo){
-        int temp = items[indexOne];
-        items[indexOne] = items[indexTwo];
-        items[indexTwo] = temp;
+    private void swap(int i, int j){
+        int temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
     }
 
     private void ensureCapacity(){
         if (size == capacity){
-            items = Arrays.copyOf(items, capacity * 2);
-            capacity *= 2;
+            capacity *= 1.5;
+            items = Arrays.copyOf(items, capacity);
         }
     }
 
@@ -35,12 +43,12 @@ class Heap{
         return items[0];
     }
 
-    public int poll(){
+    public int remove(){
         if (size == 0) throw new IllegalArgumentException();
         int item = items[0];
         items[0] = items[size - 1];
         size--;
-        heapifyDown();
+        bubbleDown();
         return item;
     }
 
@@ -48,38 +56,38 @@ class Heap{
         ensureCapacity();
         items[size] = item;
         size++;
-        heapifyUp();
+        bubbleUp();
     }
 
-    private void heapifyUp() {
-        int index = size - 1;
-        while(hasParent(index) && getParentValue(index) > items[index]){
-            swap(getParentIndex(index), index);
-            index = getParentIndex(index);
+    private void bubbleUp() {
+        int i = size - 1;
+        while(hasParent(i) && getParentValue(i) > items[i]){
+            swap(getParentIndex(i), i);
+            i = getParentIndex(i);
         }
     }
 
-    private void heapifyDown() {
-        int index = 0;
-        while (hasLeftChild(index)){ // no right child if there is no left child!
-            int smallerChildIndex = getLeftChildIndex(index); // assumption, will fix it bellow :)
-            if (hasRightChild(index) && getRightChildValue(index) < getLeftChildValue(index)){
-                smallerChildIndex = getRightChildIndex(index);
+    private void bubbleDown() {
+        int i = 0;
+        while (hasLeftChild(i)){ // no right child if there is no left child!
+            int smallerChildIndex = getLeftChildIndex(i); // assumption, will fix it bellow :)
+            if (hasRightChild(i) && getRightChildValue(i) < getLeftChildValue(i)){
+                smallerChildIndex = getRightChildIndex(i);
             }
 
-            if (items[index] < items[smallerChildIndex]){
+            if (items[i] < items[smallerChildIndex]){
                 break;
             }else{
-                swap(index, smallerChildIndex);
+                swap(i, smallerChildIndex);
             }
-            index = smallerChildIndex;
+            i = smallerChildIndex;
         }
     }
 }
 
 public class HeapExample {
     public static void main(String[]args){
-        Heap h = new Heap();
+        Heap h = new Heap(10);
         h.add(2);
         h.add(8);
         h.add(7);
@@ -91,22 +99,24 @@ public class HeapExample {
         h.add(3);
         h.add(1);
 
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
-        System.out.println("min: " + h.poll());
+        System.out.println("peek: " + h.peek());
+
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
+        System.out.println("min: " + h.remove());
     }
 }
 
-/*
+/* OUTPUT
 
-========== OUTPUT:
+peek: 0
 min: 0
 min: 1
 min: 2
