a4025727ce863a16e4b1dc31be9de2071ae1e86f
Brian Spinos <brianspinos777@hotmail.com>
2020-12-02 14:03:17 -0700

    Update python-gotchas.py

--- _scripting_languages/Python/data-structures-2020/python-gotchas.py
+++ _scripting_languages/Python/data-structures-2020/python-gotchas.py
@@ -1,7 +1,10 @@
 import array
-arr = array.array('i', [0] * 10)
+arr = array.array('i', [0] * 5)
 arr[1] = 11
 arr[2] = 22
 arr[3] = 33
+arr.insert(4,44)
+
+arr += array.array('i', [0] * 5)
 print(arr)
-# array('i', [0, 11, 22, 33, 0, 0, 0, 0, 0, 0])
+# array('i', [0, 11, 22, 33, 44, 0, 0, 0, 0, 0, 0])
