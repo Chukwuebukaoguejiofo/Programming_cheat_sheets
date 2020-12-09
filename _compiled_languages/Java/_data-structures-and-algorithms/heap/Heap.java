29f8c0e4adb4e637c920e6950c77451b0f1d8d3d
Brian Spinos <brianspinos777@hotmail.com>
2020-12-09 16:23:57 -0700

    Update Heap.java

--- _compiled_languages/Java/_data-structures-and-algorithms/heap/Heap.java
+++ _compiled_languages/Java/_data-structures-and-algorithms/heap/Heap.java
@@ -2,49 +2,48 @@ package com.example;
 
 import java.util.Arrays;
 
-class Heap{
+class Heap {
     private int capacity;
     private int size;
     int[] items;
 
-    public Heap(int capacity){
+    public Heap(int capacity) {
         this.capacity = capacity;
         size = 0;
         items = new int[capacity];
     }
 
-    private int getLeftChildIndex(int parentIndex){ return 2 * parentIndex + 1; }
-    private int getRightChildIndex(int parentIndex){ return 2 * parentIndex + 2; }
-    private int getParentIndex(int childIndex){ return (childIndex - 1) / 2; }
+    private int LI(int i) { return 2 * i + 1; }
+    private int RI(int i) { return 2 * i + 2; }
+    private int PI(int i) { return (i - 1) / 2; }
+    private boolean hasL(int i) { return LI(i) < size; }
+    private boolean hasR(int i) { return RI(i) < size; }
+    private boolean hasP(int i) { return i > 0; } // HACK!
+    private int LV(int i) { return items[LI(i)]; }
+    private int RV(int i) { return items[RI(i)]; }
+    private int PV(int i) { return items[PI(i)]; }
+    public boolean isEmpty() { return size == 0; }
 
-    private boolean hasLeftChild(int i){ return getLeftChildIndex(i) < size; }
-    private boolean hasRightChild(int i){ return getRightChildIndex(i) < size; }
-    private boolean hasParent(int i){ return i > 0; } // HACK!
-
-    private int getLeftChildValue(int i){ return items[getLeftChildIndex(i)]; }
-    private int getRightChildValue(int i){ return items[getRightChildIndex(i)]; }
-    private int getParentValue(int i){ return items[getParentIndex(i)]; }
-
-    private void swap(int i, int j){
+    private void swap(int i, int j) {
         int temp = items[i];
         items[i] = items[j];
         items[j] = temp;
     }
 
-    private void ensureCapacity(){
-        if (size == capacity){
+    private void ensureCapacity() {
+        if (size == capacity) {
             capacity *= 1.5;
             items = Arrays.copyOf(items, capacity);
         }
     }
 
-    public int peek(){
-        if (size == 0) throw new IllegalArgumentException();
+    public int peek() throws Exception {
+        if (size == 0) throw new Exception("Heap is empty");
         return items[0];
     }
 
-    public int remove(){
-        if (size == 0) throw new IllegalArgumentException();
+    public int remove() throws Exception {
+        if (size == 0) throw new Exception("Heap is empty");
         int item = items[0];
         items[0] = items[size - 1];
         size--;
@@ -52,7 +51,7 @@ class Heap{
         return item;
     }
 
-    public void add(int item){
+    public void add(int item) {
         ensureCapacity();
         items[size] = item;
         size++;
@@ -61,56 +60,61 @@ class Heap{
 
     private void bubbleUp() {
         int i = size - 1;
-        while(hasParent(i) && getParentValue(i) > items[i]){
-            swap(getParentIndex(i), i);
-            i = getParentIndex(i);
+        while (hasP(i) && PV(i) > items[i]) {
+            swap(PI(i), i);
+            i = PI(i);
         }
     }
 
     private void bubbleDown() {
         int i = 0;
-        while (hasLeftChild(i)){ // no right child if there is no left child!
-            int smallerChildIndex = getLeftChildIndex(i); // assumption, will fix it bellow :)
-            if (hasRightChild(i) && getRightChildValue(i) < getLeftChildValue(i)){
-                smallerChildIndex = getRightChildIndex(i);
-            }
+        while (hasL(i)) { // no right child if there is no left child!
+            int smallerChildIndex = LI(i); // assumption, will fix it bellow
+            if (hasR(i) && RV(i) < LV(i))
+                smallerChildIndex = RI(i);
 
-            if (items[i] < items[smallerChildIndex]){
-                break;
-            }else{
+            if (items[i] > items[smallerChildIndex]) {
                 swap(i, smallerChildIndex);
+                i = smallerChildIndex;
+            } else {
+                break;
             }
-            i = smallerChildIndex;
         }
     }
 }
 
 public class HeapExample {
-    public static void main(String[]args){
-        Heap h = new Heap(10);
-        h.add(2);
-        h.add(8);
-        h.add(7);
-        h.add(6);
-        h.add(5);
-        h.add(0);
-        h.add(9);
-        h.add(4);
-        h.add(3);
-        h.add(1);
+    public static void main(String[] args) {
+        Heap heap = new Heap(10);
+        heap.add(2);
+        heap.add(8);
+        heap.add(7);
+        heap.add(6);
+        heap.add(5);
+        heap.add(0);
+        heap.add(9);
+        heap.add(4);
+        heap.add(3);
+        heap.add(1);
+        try {
+            if (!heap.isEmpty()) {
+                System.out.println("peek: " + heap.peek());
+            }
 
-        System.out.println("peek: " + h.peek());
-
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
-        System.out.println("min: " + h.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            System.out.println("min: " + heap.remove());
+            //System.out.println("min: " + heap.remove()); // ERROR: Heap is empty
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
 }
 
