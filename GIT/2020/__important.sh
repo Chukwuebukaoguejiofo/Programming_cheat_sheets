8ed159490100224f99718b2c3bbd08d839736343
Brian Spinos <brianspinos777@hotmail.com>
2021-02-01 09:58:42 -0700

    Update __important.sh

--- GIT/2020/__important.sh
+++ GIT/2020/__important.sh
@@ -3,3 +3,10 @@ git reset --hard HEAD~2
 
 # undoes the changes from commit abc123, creating a new commit with the changes
 git revert abc123
+
+
+# Undo commit
+git reset --soft HEAD~1
+
+# undo git add
+git reset foo.txt
