2d67182ce78da1bfaaa8df30f7201a6ca4d95449
Brian Spinos <brianspinos777@hotmail.com>
2021-04-05 14:54:18 -0700

    Update __important.sh

--- GIT/2020/__important.sh
+++ GIT/2020/__important.sh
@@ -10,3 +10,15 @@ git reset --soft HEAD~1
 
 # undo git add
 git reset foo.txt
+
+
+#
+# commit unwanted changes, then restore them back to original state:
+#
+// keep track of your original branch name: example: [feature/foobar-branch]
+$ git add .
+$ gcm "DONT_USE_THIS_COMMIT"
+// go to the other branch u want to go to do your stuff...
+// now get back to your original branch and do:
+$ git reset --mixed <SHA> # SHA of the commit before your "DONT_USE_THIS_COMMIT" -- seems that even the untracked files go back to be untracked!!!
+
