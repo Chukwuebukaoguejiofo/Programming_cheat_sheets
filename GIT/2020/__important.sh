f19bd3856eac055eda532504f64ce5c073285a26
Brian Spinos <brianspinos777@hotmail.com>
2021-04-13 16:09:18 -0700

    Update __important.sh

--- GIT/2020/__important.sh
+++ GIT/2020/__important.sh
@@ -20,5 +20,5 @@ $ git add .
 $ gcm "DONT_USE_THIS_COMMIT"
 // go to the other branch u want to go to do your stuff...
 // now get back to your original branch and do:
-$ git reset --mixed <SHA> # SHA of the commit before your "DONT_USE_THIS_COMMIT" -- seems that even the untracked files go back to be untracked!!!
+$ git reset --mixed <SHA> # SHA of the commit ****BEFORE**** your "DONT_USE_THIS_COMMIT" -- seems that even the untracked files go back to be untracked!!!
 
