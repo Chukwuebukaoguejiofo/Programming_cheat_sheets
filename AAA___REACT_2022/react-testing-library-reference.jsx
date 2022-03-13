e442461b1a01c2f6bc75463b76456e6828626896
Brian Spinos <brianspinos777@hotmail.com>
2022-03-13 08:46:55 -0700

    Update react-testing-library-reference.jsx

--- AAA___REACT_2022/react-testing-library-reference.jsx
+++ AAA___REACT_2022/react-testing-library-reference.jsx
@@ -53,4 +53,115 @@ describe('FooBar', () => {
         let pTag = component.getByTestId('exp');
         expect(pTag).toHaveTextContent('expVal: $$$');
     });
-});//
+});
+
+
+
+
+
+
+//===================================================================
+
+import {render, waitFor, fireEvent, screen} from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import Parent from './Parent';
+
+describe('Parent', () => {
+    afterEach(() => {
+        jest.restoreAllMocks(); // cleanup
+    });
+
+    it('normal fetch should work', async () => {
+        const component = render(<Parent />);
+        const pTag = component.getByTestId("user-details");
+
+        // await, waitFor, expect
+        await waitFor(() => {
+          expect(pTag).toHaveTextContent("Brian 101");
+        });
+        //expect(screen.queryByText(/Loading/)).toBeNull(); // we don't have to await this one because we awaited the proper condition in the previous line
+    });
+
+    it('mock fetch', async () => {
+        const mockResponse = {name: 'Erich', id: 999};
+        jest.spyOn(global, 'fetch').mockImplementation(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve(mockResponse)
+              })
+        });
+
+        const component = render(<Parent />);
+        const pTag = await component.findByTestId("user-details");
+        expect(pTag).toHaveTextContent("Erich 999");
+    });
+
+    it('normal fetch should work #2', async () => {
+        const component = render(<Parent />);
+        const pTag = component.getByTestId("user-details");
+
+        // `waitFor` waits until the callback doesn't throw an error
+        await waitFor(() => {
+            expect(pTag).toHaveTextContent("Brian 101");
+        });
+        //expect(screen.queryByText(/Loading/)).toBeNull(); // we don't have to await this one because we awaited the proper condition in the previous line
+    });
+
+    it('usinf ACT to test fetch (needs fetch mock)', async () => {
+        let component;
+
+        const mockResponse = {name: 'AAA', id: 888};
+        jest.spyOn(global, 'fetch').mockImplementation(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve(mockResponse)
+              })
+        });
+
+        // Use the asynchronous version of act to apply resolved promises
+        // await, act, async
+        await act(async () => {
+            component = render(<Parent />);
+        });
+        
+        // component.debug();
+        const pTag = component.getByTestId("user-details");
+        expect(pTag).toHaveTextContent("AAA 888");
+    });
+
+    it('mock fetch #2', async () => {
+        const mockResponse = {name: 'Erich', id: 999};
+        jest.spyOn(global, 'fetch').mockImplementation(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve(mockResponse)
+              })
+        });
+
+        const component = render(<Parent />);
+        const pTag = await component.findByText(/Erich 999/i);
+        // const pTag = await component.findByTestId("user-details");
+        expect(pTag).toHaveTextContent("Erich 999");
+    });
+
+    it('normal fetch should work #3', async () => {
+        const component = render(<Parent />);
+
+        // wait until u see the result !!!
+        const pTag = await component.findByText(/Brian 101/i);
+        // const pTag = await component.findByTestId("user-details");
+        expect(pTag).toHaveTextContent("Brian 101");
+    });
+
+    // it.only('ONLY TEST', async () => {
+    //     //...
+    // });
+
+    it('snapshot test', async () => {
+        const component = render(<Parent />);
+
+        // wait until u see the result !!!
+        const pTag = await component.findByText(/Brian 101/i);
+        // const pTag = await component.findByTestId("user-details");
+        expect(pTag).toHaveTextContent("Brian 101");
+
+        //expect(pTag).toMatchSnapshot(); // creates a __snapshots__ folder the first time
+    });
+});
